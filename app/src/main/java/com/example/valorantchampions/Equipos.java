599f720b90249b4c049d4c2a440e5b745d6cc8bb
pecador <peco_alvaro@hotmail.com>
2023-02-07 12:48:07 +0100

    intento realizar la lista en una nueva clase

---
+++ app/src/main/java/com/example/valorantchampions/Equipos.java
@@ -0,0 +1,84 @@
+package com.example.valorantchampions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+public class Equipos extends AppCompatActivity {
+
+    TextView texto1, nombreEquipo, ciudad, puntos;
+    SQLiteHelper helper;
+    SQLiteDatabase db;
+    ListView lv;
+    ImageView foto;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equipos);
+
+        texto1 = findViewById(R.id.textView);
+        nombreEquipo = findViewById(R.id.textNombre);
+        ciudad = findViewById(R.id.textCiudad);
+        puntos = findViewById(R.id.textPuntos);
+        lv = findViewById(R.id.listaM);
+        // mostrarTabla();
+        consultaEquipos();
+    }
+
+
+    public void consultaEquipos (){
+        helper = new SQLiteHelper(this);
+        db = helper.getReadableDatabase();
+
+        Cursor cursor =
+                db.query(EstructuraBBDD.EstructuraEquipos.TABLE_NAME_EQUIPOS, null,
+                        null, null, null, null, null);
+
+        String[] from = {EstructuraBBDD.EstructuraEquipos.COLUMN_NAME_EQUIPO,
+                EstructuraBBDD.EstructuraEquipos.COLUMN_NAME_CIUDAD,
+                EstructuraBBDD.EstructuraEquipos.COLUMN_NAME_PUNTOS,
+                EstructuraBBDD.EstructuraEquipos.COLUMN_FOTO_FOTO};
+        int[] to = {R.id.textNombre, R.id.textCiudad,R.id.textPuntos, R.id.imageView};
+        SimpleCursorAdapter adaptador = new SimpleCursorAdapter(this,
+                R.layout.lista, cursor, from, to,
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        lv.setAdapter(adaptador);
+        db.close();
+    }
+
+
+    public void mostrarTabla (){
+        helper = new SQLiteHelper(this);
+        db = helper.getReadableDatabase();
+
+        Cursor cursor =
+                db.query(EstructuraBBDD.EstructuraEquipos.TABLE_NAME_EQUIPOS, null,
+                        null, null, null, null, null);
+
+        texto1.append("\n Tabla Equipos \n-----------");
+        cursor.moveToFirst();
+        int nfilas=cursor.getCount();
+        int ncolumnas=cursor.getColumnCount();
+        String fila="\n";
+        for (int i = 0; i < nfilas; i++) {
+            fila="\n";
+            for(int j=0;j<ncolumnas;j++){
+                fila=fila+cursor.getString(j)+" ";
+            }
+            texto1.append(fila);
+            cursor.moveToNext();
+        }
+
+        db.close();
+    }
+}
--- app/src/main/java/com/example/valorantchampions/MainActivity.java
+++ app/src/main/java/com/example/valorantchampions/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.valorantchampions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
         helper=new SQLiteHelper(this);
         db = helper.getWritableDatabase();
+        db.delete("Equipos", "_id>0", null);
+        crearEquipos();
 
 
+    }
 
+    public void crearEquipos(){
+        insertaEquipos("boom", "Indonesia", -15, R.drawable.boom);
+        insertaEquipos("cienthieves", "Estados Unidos", -17, R.drawable.cienthieves);
+        insertaEquipos("drx", "Korea", 17, R.drawable.drx);
+        insertaEquipos("Edward Gaming", "China", -9, R.drawable.edward);
+        insertaEquipos("Fnatic", "Europa", 15, R.drawable.fnatic);
+        insertaEquipos("Fun Plus Phoenix", "Europa", 13, R.drawable.fun);
+        insertaEquipos("Furia eSport", "Brasil", -15, R.drawable.furia);
+        insertaEquipos("Kru eSport", "Latam", -8, R.drawable.kru);
+        insertaEquipos("Leviatán", "Latam", 13, R.drawable.leviatan);
+        insertaEquipos("Team Liquid", "Europa", 2, R.drawable.liquid);
+        insertaEquipos("LOUD", "Brasil", 12, R.drawable.loud);
+        insertaEquipos("Optic Gaming", "NorteAmerica", 23, R.drawable.optic);
+        insertaEquipos("Paper Rex", "Asia", -6, R.drawable.paper);
+        insertaEquipos("xSet", "Estados Unidos", 10, R.drawable.set);
+        insertaEquipos("xerxia", "Tailandia", -15, R.drawable.xerxia);
+        insertaEquipos("Zeta Division", "Japon", -8, R.drawable.zeta);
     }
 
     public void insertaEquipos(String equipo, String ciudad, int puntos, int foto){
@@ -43,5 +65,10 @@ public class MainActivity extends AppCompatActivity {
         values.put("Puntuacion1", puntuacion1);
         values.put("Puntuacion2", puntuacion2);
         db.insert("Encuentros", null, values);
+    }
+
+    public void entrarEquipos(View view){
+        Intent i = new Intent(this, Equipos.class);
+        startActivity(i);
     }
 }
